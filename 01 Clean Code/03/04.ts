8c3fb2d8d1e282a2094daf5dbc499eac3cd03b4c
Michal Radomski <michalra@op.pl>
2023-12-11 00:34:02 +0100

    Course - commit 17

--- 01 Clean Code/03/04.ts
+++ 01 Clean Code/03/04.ts
@@ -1,60 +1,98 @@
 //* Classes & Polymorphism
-type Purchase = any;
-let Logistics: any;
+// type Purchase = any;
+// let Logistics: any;
 
-interface Delivery {
-  deliverProduct(): void;
-  trackProduct(): void;
+// interface Delivery {
+//   deliverProduct(): void;
+//   trackProduct(): void;
+// }
+
+// class DeliveryImplementation {
+//   protected purchase: Purchase;
+
+//   constructor(purchase: Purchase) {
+//     this.purchase = purchase;
+//   }
+// }
+
+// class ExpressDelivery extends DeliveryImplementation implements Delivery {
+//   deliverProduct() {
+//     Logistics.issueExpressDelivery(this.purchase.product);
+//   }
+
+//   trackProduct() {
+//     Logistics.trackExpressDelivery(this.purchase.product);
+//   }
+// }
+
+// class InsuredDelivery extends DeliveryImplementation implements Delivery {
+//   deliverProduct() {
+//     Logistics.issueInsuredDelivery(this.purchase.product);
+//   }
+
+//   trackProduct() {
+//     Logistics.trackInsuredDelivery(this.purchase.product);
+//   }
+// }
+
+// class StandardDelivery extends DeliveryImplementation implements Delivery {
+//   deliverProduct() {
+//     Logistics?.issueStandardDelivery(this?.purchase?.product);
+//   }
+
+//   trackProduct() {
+//     Logistics.trackStandardDelivery(this.purchase.product);
+//   }
+// }
+
+// function createDelivery(purchase: { deliveryType?: string }) {
+//   if (purchase.deliveryType === "express") {
+//     return new ExpressDelivery(purchase);
+//   } else if (purchase.deliveryType === "insured") {
+//     return new InsuredDelivery(purchase);
+//   } else {
+//     return new StandardDelivery(purchase);
+//   }
+// }
+
+// const delivery: Delivery = createDelivery({});
+// delivery.deliverProduct();
+// console.log("delivery:", delivery);
+
+//* Clean Classes Should Be Small -> Single Responsibility Principle
+class Order {
+  public refund() {}
 }
 
-class DeliveryImplementation {
-  protected purchase: Purchase;
+class Customer {
+  private orders: Order[];
 
-  constructor(purchase: Purchase) {
-    this.purchase = purchase;
+  constructor(email: string, password: string, orders: Order[]) {
+    this.orders = orders;
   }
+
+  public login(email: string, password: string) {}
+
+  public updateProfile(name: string) {}
+
+  public makePurchase(productId: string) {}
 }
 
-class ExpressDelivery extends DeliveryImplementation implements Delivery {
-  deliverProduct() {
-    Logistics.issueExpressDelivery(this.purchase.product);
-  }
+class Product {
+  constructor(title: string, price: number) {}
 
-  trackProduct() {
-    Logistics.trackExpressDelivery(this.purchase.product);
-  }
+  public update(Id: string, title: string, price: number) {}
+
+  public remove(Id: string) {}
 }
 
-class InsuredDelivery extends DeliveryImplementation implements Delivery {
-  deliverProduct() {
-    Logistics.issueInsuredDelivery(this.purchase.product);
+class Inventory {
+  private products: Product;
+  constructor(products: Product) {
+    this.products = products;
   }
 
-  trackProduct() {
-    Logistics.trackInsuredDelivery(this.purchase.product);
-  }
+  public getAvailableItems(productId: string) {}
+
+  public restockProduct(productId: string) {}
 }
-
-class StandardDelivery extends DeliveryImplementation implements Delivery {
-  deliverProduct() {
-    Logistics?.issueStandardDelivery(this?.purchase?.product);
-  }
-
-  trackProduct() {
-    Logistics.trackStandardDelivery(this.purchase.product);
-  }
-}
-
-function createDelivery(purchase: { deliveryType?: string }) {
-  if (purchase.deliveryType === "express") {
-    return new ExpressDelivery(purchase);
-  } else if (purchase.deliveryType === "insured") {
-    return new InsuredDelivery(purchase);
-  } else {
-    return new StandardDelivery(purchase);
-  }
-}
-
-const delivery: Delivery = createDelivery({ deliveryType: "testDelivery" });
-delivery.deliverProduct();
-console.log("delivery:", delivery);
