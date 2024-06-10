769ef5680125439670ac1f512d2811ca0c81c09b
Michal Radomski <michalra@op.pl>
2024-06-11 01:16:03 +0200

    Desing Patterns in TS - commit 28

--- 02 Design Patterns in TS/01_creational_patterns/01_factory.ts
+++ 02 Design Patterns in TS/01_creational_patterns/01_factory.ts
@@ -1,51 +1,52 @@
 //* Factory Pattern
 // The Factory pattern is really about adding that extra abstraction between the object creation and where it is used. This gives you extra options that you can more easily extend in the future.
 
-// interface IProduct {
-//   name: string;
-// }
+interface IProduct {
+  name: string;
+}
 
-// class ConcreteProduct implements IProduct {
-//   name = "";
-// }
+class ConcreteProduct implements IProduct {
+  name = "";
+}
 
-// class ConcreteProductA extends ConcreteProduct {
-//   constructor() {
-//     super();
-//     this.name = "ConcreteProductA";
-//   }
-// }
+class ConcreteProductA extends ConcreteProduct {
+  constructor() {
+    super();
+    this.name = "ConcreteProductA";
+  }
+}
 
-// class ConcreteProductB extends ConcreteProduct {
-//   constructor() {
-//     super();
-//     this.name = "ConcreteProductB";
-//   }
-// }
+class ConcreteProductB extends ConcreteProduct {
+  constructor() {
+    super();
+    this.name = "ConcreteProductB";
+  }
+}
 
-// class ConcreteProductC extends ConcreteProduct {
-//   constructor() {
-//     super();
-//     this.name = "ConcreteProductC";
-//   }
-// }
+class ConcreteProductC extends ConcreteProduct {
+  constructor() {
+    super();
+    this.name = "ConcreteProductC";
+  }
+}
 
-// class Creator {
-//   static createObject(someProperty: string): IProduct {
-//     if (someProperty === "a") {
-//       return new ConcreteProductA();
-//     } else if (someProperty === "b") {
-//       return new ConcreteProductB();
-//     } else {
-//       return new ConcreteProductC();
-//     }
-//   }
-// }
+class Creator {
+  static createObject(someProperty: string): IProduct {
+    if (someProperty === "a") {
+      return new ConcreteProductA();
+    } else if (someProperty === "b") {
+      return new ConcreteProductB();
+    } else {
+      return new ConcreteProductC();
+    }
+  }
+}
 
-// // The Client
-// const product = Creator.createObject("b");
-// console.log(product.name); // ConcreteProductB
+// The Client
+const product = Creator.createObject("b");
+console.log(product.name); // ConcreteProductB
 
+//* Factory Use Case Example
 type dimension = {
   height: number;
   width: number;
--- 02 Design Patterns in TS/01_creational_patterns/02_abstract_factory.ts
+++ 02 Design Patterns in TS/01_creational_patterns/02_abstract_factory.ts
@@ -1,3 +1,4 @@
+export {};
 //* Abstract Factory Pattern
 // The Abstract Factory Pattern adds an abstraction layer over multiple other creational pattern implementations.
 
@@ -127,3 +128,179 @@ console.log("product_1:", product_1);
 
 const product_2 = AbstractFactory.createObject("bc");
 console.log("product_2:", product_2);
+
+//* Abstract Factory Use Case Example
+type dimension = {
+  height: number;
+  width: number;
+  depth: number;
+};
+
+interface IChair {
+  name: string;
+  height: number;
+  width: number;
+  depth: number;
+
+  getDimensions(): dimension;
+}
+
+class Chair implements IChair {
+  name = "";
+  height = 0;
+  width = 0;
+  depth = 0;
+
+  getDimensions(): dimension {
+    return {
+      width: this.width,
+      depth: this.depth,
+      height: this.height,
+    };
+  }
+}
+
+class ChairFactory {
+  static getChair(chair: string): IChair {
+    if (chair == "BigChair") {
+      return new BigChair();
+    } else if (chair == "MediumChair") {
+      return new MediumChair();
+    } else if (chair == "SmallChair") {
+      return new SmallChair();
+    } else {
+      throw new Error("No Chair Found");
+    }
+  }
+}
+
+class SmallChair extends Chair {
+  constructor() {
+    super();
+    this.name = "SmallChair";
+    this.height = 40;
+    this.width = 40;
+    this.depth = 40;
+  }
+}
+
+class MediumChair extends Chair {
+  constructor() {
+    super();
+    this.name = "MediumChair";
+    this.height = 60;
+    this.width = 60;
+    this.depth = 60;
+  }
+}
+
+class BigChair extends Chair {
+  constructor() {
+    super();
+    this.name = "BigChair";
+    this.height = 80;
+    this.width = 80;
+    this.depth = 80;
+  }
+}
+
+interface ITable {
+  name: string;
+  height: number;
+  width: number;
+  depth: number;
+
+  getDimensions(): dimension;
+}
+
+class Table implements ITable {
+  name = "";
+  height = 0;
+  width = 0;
+  depth = 0;
+
+  getDimensions(): dimension {
+    return {
+      width: this.width,
+      depth: this.depth,
+      height: this.height,
+    };
+  }
+}
+
+class TableFactory {
+  static getTable(table: string): ITable {
+    if (table === "BigTable") {
+      return new BigTable();
+    } else if (table === "MediumTable") {
+      return new MediumTable();
+    } else if (table === "SmallTable") {
+      return new SmallTable();
+    } else {
+      throw new Error("No Table Found");
+    }
+  }
+}
+
+class SmallTable extends Table {
+  constructor() {
+    super();
+    this.name = "SmallTable";
+    this.height = 40;
+    this.width = 40;
+    this.depth = 40;
+  }
+}
+
+class MediumTable extends Table {
+  constructor() {
+    super();
+    this.name = "MediumTable";
+    this.height = 60;
+    this.width = 60;
+    this.depth = 60;
+  }
+}
+
+class BigTable extends Table {
+  constructor() {
+    super();
+    this.name = "BigTable";
+    this.height = 80;
+    this.width = 80;
+    this.depth = 80;
+  }
+}
+
+interface IFurniture extends IChair, ITable {}
+
+class FurnitureFactory {
+  static getFurniture(furniture: string): IFurniture | undefined {
+    try {
+      if (["SmallChair", "MediumChair", "BigChair"].indexOf(furniture) > -1) {
+        return ChairFactory.getChair(furniture);
+      }
+      if (["SmallTable", "MediumTable", "BigTable"].indexOf(furniture) > -1) {
+        return TableFactory.getTable(furniture);
+      }
+      throw new Error("No Factory Found");
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+// The Client
+
+const furniture_1 = FurnitureFactory.getFurniture("SmallChair");
+console.log(
+  "furniture_1?.name, JSON.stringify(furniture_1?.getDimensions():",
+  furniture_1?.name,
+  JSON.stringify(furniture_1?.getDimensions())
+);
+const furniture_2 = FurnitureFactory.getFurniture("SmallChair");
+console.log(
+  "furniture_2?.name, JSON.stringify(furniture_2?.getDimensions():",
+  furniture_2?.name,
+  JSON.stringify(furniture_2?.getDimensions())
+);
