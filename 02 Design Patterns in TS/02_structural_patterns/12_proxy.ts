cf7ca7373293cb982f7e873066bd1b4949d3f01e
Michal Radomski <michalra@op.pl>
2024-06-13 01:47:15 +0200

    Desing Patterns in TS - commit 50

--- 02 Design Patterns in TS/02_structural_patterns/12_proxy.ts
+++ 02 Design Patterns in TS/02_structural_patterns/12_proxy.ts
@@ -56,3 +56,98 @@ console.log("response:", response);
 // Use the Subject again, but this time it retrieves it from the local cache
 response = JSON.stringify(PROXY_SUBJECT.request());
 console.log("response:", response);
+
+//* A Proxy Use Case Example
+interface IProteus {
+  // A Greek mythological character that can change to many forms
+
+  tellMeTheFuture(): void;
+  // Proteus will change form rather than tell you the future
+
+  tellMeYourForm(): void;
+  // The form of Proteus is elusive like the sea
+}
+
+class Lion implements IProteus {
+  // Proteus in the form of a Lion
+
+  name = "Lion";
+
+  tellMeTheFuture(): void {
+    // Proteus will change to something random
+    if (Math.floor(Math.random() * 2)) {
+      Object.assign(this, new Serpent());
+      this.tellMeTheFuture = Serpent.prototype.tellMeTheFuture;
+      this.tellMeYourForm = Serpent.prototype.tellMeYourForm;
+    } else {
+      Object.assign(this, new Leopard());
+      this.tellMeTheFuture = Leopard.prototype.tellMeTheFuture;
+      this.tellMeYourForm = Leopard.prototype.tellMeYourForm;
+    }
+  }
+
+  tellMeYourForm(): void {
+    console.log(`I am the form of ${this.name}`);
+  }
+}
+
+class Serpent implements IProteus {
+  // Proteus in the form of a Serpent
+
+  name = "Serpent";
+
+  tellMeTheFuture(): void {
+    // Proteus will change to something random
+    if (Math.floor(Math.random() * 2)) {
+      Object.assign(this, new Leopard());
+      this.tellMeTheFuture = Leopard.prototype.tellMeTheFuture;
+      this.tellMeYourForm = Leopard.prototype.tellMeYourForm;
+    } else {
+      Object.assign(this, new Lion());
+      this.tellMeTheFuture = Lion.prototype.tellMeTheFuture;
+      this.tellMeYourForm = Lion.prototype.tellMeYourForm;
+    }
+  }
+
+  tellMeYourForm(): void {
+    console.log(`I am the form of ${this.name}`);
+  }
+}
+
+class Leopard implements IProteus {
+  // Proteus in the form of a Leopard
+
+  name = "Leopard";
+
+  tellMeTheFuture(): void {
+    // Proteus will change to something random
+    if (Math.floor(Math.random() * 2)) {
+      Object.assign(this, new Lion());
+      this.tellMeTheFuture = Lion.prototype.tellMeTheFuture;
+      this.tellMeYourForm = Lion.prototype.tellMeYourForm;
+    } else {
+      Object.assign(this, new Serpent());
+      this.tellMeTheFuture = Serpent.prototype.tellMeTheFuture;
+      this.tellMeYourForm = Serpent.prototype.tellMeYourForm;
+    }
+  }
+
+  tellMeYourForm(): void {
+    console.log(`I am the form of ${this.name}`);
+  }
+}
+
+const PROTEUS = new Lion();
+PROTEUS.tellMeYourForm();
+PROTEUS.tellMeTheFuture();
+PROTEUS.tellMeYourForm();
+PROTEUS.tellMeTheFuture();
+PROTEUS.tellMeYourForm();
+PROTEUS.tellMeTheFuture();
+PROTEUS.tellMeYourForm();
+PROTEUS.tellMeTheFuture();
+PROTEUS.tellMeYourForm();
+PROTEUS.tellMeTheFuture();
+PROTEUS.tellMeYourForm();
+PROTEUS.tellMeTheFuture();
+PROTEUS.tellMeYourForm();
