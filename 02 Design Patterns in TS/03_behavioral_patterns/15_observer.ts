2ed1994a8db91e1787fbd1e32e133c1820211cd7
Michal Radomski <michalra@op.pl>
2024-06-14 01:53:39 +0200

    Desing Patterns in TS - commit 57

--- 02 Design Patterns in TS/03_behavioral_patterns/15_observer.ts
+++ 02 Design Patterns in TS/03_behavioral_patterns/15_observer.ts
@@ -2,75 +2,75 @@ export {};
 //* Observer Pattern
 // The Observer pattern is a software design pattern in which an object, called the Subject (Observable), manages a list of dependents, called Observers, and notifies them automatically of any internal state changes by calling one of their method
 
-// interface IObservable {
-//   // The Subject Interface
+interface IObservable {
+  // The Subject Interface
 
-//   subscribe(observer: IObserver): void;
-//   // The subscribe method
+  subscribe(observer: IObserver): void;
+  // The subscribe method
 
-//   unsubscribe(observer: IObserver): void;
-//   // The unsubscribe method
+  unsubscribe(observer: IObserver): void;
+  // The unsubscribe method
 
-//   notify(...args: unknown[]): void;
-//   // The notify method
-// }
+  notify(...args: unknown[]): void;
+  // The notify method
+}
 
-// class Subject implements IObservable {
-//   // The Subject (a.k.a Observable)
-//   #observers: Set<IObserver>;
-//   constructor() {
-//     this.#observers = new Set();
-//   }
+class Subject implements IObservable {
+  // The Subject (a.k.a Observable)
+  #observers: Set<IObserver>;
+  constructor() {
+    this.#observers = new Set();
+  }
 
-//   subscribe(observer: IObserver) {
-//     this.#observers.add(observer);
-//   }
+  subscribe(observer: IObserver) {
+    this.#observers.add(observer);
+  }
 
-//   unsubscribe(observer: IObserver) {
-//     this.#observers.delete(observer);
-//   }
+  unsubscribe(observer: IObserver) {
+    this.#observers.delete(observer);
+  }
 
-//   notify(...args: unknown[]) {
-//     this.#observers.forEach((observer) => {
-//       observer.notify(...args);
-//     });
-//   }
-// }
+  notify(...args: unknown[]) {
+    this.#observers.forEach((observer) => {
+      observer.notify(...args);
+    });
+  }
+}
 
-// interface IObserver {
-//   // A method for the Observer to implement
+interface IObserver {
+  // A method for the Observer to implement
 
-//   notify(...args: unknown[]): void;
-//   // Receive notifications"
-// }
+  notify(...args: unknown[]): void;
+  // Receive notifications"
+}
 
-// class Observer implements IObserver {
-//   // The concrete observer
-//   #id: number;
+class Observer implements IObserver {
+  // The concrete observer
+  #id: number;
 
-//   constructor(observable: IObservable) {
-//     this.#id = COUNTER++;
-//     observable.subscribe(this);
-//   }
+  constructor(observable: IObservable) {
+    this.#id = COUNTER++;
+    observable.subscribe(this);
+  }
 
-//   notify(...args: unknown[]) {
-//     console.log(`OBSERVER_${this.#id} received ${JSON.stringify(args)}`);
-//   }
-// }
+  notify(...args: unknown[]) {
+    console.log(`OBSERVER_${this.#id} received ${JSON.stringify(args)}`);
+  }
+}
 
-// // The Client
-// let COUNTER = 1; // An ID to help distinguish between objects
+// The Client
+let COUNTER = 1; // An ID to help distinguish between objects
 
-// const SUBJECT = new Subject();
-// const OBSERVER_1 = new Observer(SUBJECT);
-// const OBSERVER_2 = new Observer(SUBJECT);
+const SUBJECT = new Subject();
+const OBSERVER_1 = new Observer(SUBJECT);
+const OBSERVER_2 = new Observer(SUBJECT);
 
-// SUBJECT.notify("First Notification", [1, 2, 3]);
+SUBJECT.notify("First Notification", [1, 2, 3]);
 
-// // Unsubscribe OBSERVER_2
-// SUBJECT.unsubscribe(OBSERVER_2);
+// Unsubscribe OBSERVER_2
+SUBJECT.unsubscribe(OBSERVER_2);
 
-// SUBJECT.notify("Second Notification", { A: 1, B: 2, C: 3 });
+SUBJECT.notify("Second Notification", { A: 1, B: 2, C: 3 });
 
 //* Observer Design Pattern Use Case
 interface IDataController {
