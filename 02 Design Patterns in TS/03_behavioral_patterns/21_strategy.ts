26962370b638badeff30711e0aaa597ae7290878
Michal Radomski <michalra@op.pl>
2024-06-16 00:44:58 +0200

    Desing Patterns in TS - commit 67

---
+++ 02 Design Patterns in TS/03_behavioral_patterns/21_strategy.ts
@@ -0,0 +1,53 @@
+export {};
+//* Strategy Design Pattern
+// The Strategy Pattern is similar to the State Pattern, except that the client passes in the algorithm that the context should run.
+
+class ObjectContext {
+  // This is the object whose behavior will change
+
+  request(strategy: IStrategyConstructor) {
+    // The request is handled by the class passed in
+    return new strategy();
+  }
+}
+
+interface IStrategyConstructor {
+  // A Constructor for the IStrategy
+  new (): IStrategy;
+}
+
+interface IStrategy {
+  // A strategy Interface
+  method(): string;
+}
+
+class ConcreteStrategyA implements IStrategy {
+  // A Concrete Strategy Subclass
+
+  method() {
+    return "I am ConcreteStrategyA";
+  }
+}
+
+class ConcreteStrategyB implements IStrategy {
+  // A Concrete Strategy Subclass
+
+  method() {
+    return "I am ConcreteStrategyB";
+  }
+}
+
+class ConcreteStrategyC implements IStrategy {
+  // A Concrete Strategy Subclass
+
+  method() {
+    return "I am ConcreteStrategyC";
+  }
+}
+
+// The Client
+const OBJECT_CONTEXT = new ObjectContext();
+
+console.log(OBJECT_CONTEXT.request(ConcreteStrategyA).method());
+console.log(OBJECT_CONTEXT.request(ConcreteStrategyB).method());
+console.log(OBJECT_CONTEXT.request(ConcreteStrategyC).method());
