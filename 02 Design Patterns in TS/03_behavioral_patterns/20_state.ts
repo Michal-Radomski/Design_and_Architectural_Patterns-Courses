97e92ae8593eb466f0d7ef99f61e1e71125a60d4
Michal Radomski <michalra@op.pl>
2024-06-16 00:00:39 +0200

    Desing Patterns in TS - commit 65

---
+++ 02 Design Patterns in TS/03_behavioral_patterns/20_state.ts
@@ -0,0 +1,58 @@
+export {};
+//* State Design Pattern
+// The State Pattern is more concerned about changing the handle of an object's method dynamically. This makes an object itself more dynamic and may reduce the need of many conditional statements.
+
+class Context {
+  // This is the object whose behavior will change
+  #stateHandles: IState[];
+  #handle: IState | undefined;
+
+  constructor() {
+    this.#stateHandles = [new ConcreteStateA(), new ConcreteStateB(), new ConcreteStateC()];
+    this.#handle = undefined;
+  }
+
+  request() {
+    // A method of the state that dynamically changes which
+    // class it uses depending on the value of this.#handle
+    this.#handle = this.#stateHandles[Math.floor(Math.random() * 3)];
+    return this.#handle;
+  }
+}
+
+interface IState {
+  // A State Interface
+  toString(): string;
+}
+
+class ConcreteStateA implements IState {
+  // A ConcreteState Subclass
+
+  toString() {
+    return "I am ConcreteStateA";
+  }
+}
+
+class ConcreteStateB implements IState {
+  // A ConcreteState Subclass
+
+  toString() {
+    return "I am ConcreteStateB";
+  }
+}
+
+class ConcreteStateC implements IState {
+  // A ConcreteState Subclass
+
+  toString() {
+    return "I am ConcreteStateC";
+  }
+}
+
+// The Client
+const CONTEXT = new Context();
+console.log(CONTEXT.request());
+console.log(CONTEXT.request());
+console.log(CONTEXT.request());
+console.log(CONTEXT.request());
+console.log(CONTEXT.request());
