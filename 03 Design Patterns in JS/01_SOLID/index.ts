6c0bfa219a4fcb8ca6b2e2ed8e0ba9bdf85b6515
Michal Radomski <michalra@op.pl>
2024-06-16 15:45:05 +0200

    Design Patterns in JS - commit 02

--- 03 Design Patterns in JS/01_SOLID/index.ts
+++ 03 Design Patterns in JS/01_SOLID/index.ts
@@ -1,11 +1,11 @@
-//* Single Responsibility Principle
-
 import fs from "fs";
 
 interface ObjectI {
   [key: string]: string | number | ObjectI;
 }
 
+//^ 1. Single Responsibility Principle
+console.log("-----------------");
 class Journal {
   entries: ObjectI;
   static count: number;
@@ -50,11 +50,125 @@ class PersistenceManager {
   }
 }
 
-let j = new Journal();
+const j = new Journal();
 j.addEntry("I cried today.");
 j.addEntry("I ate a bug.");
 console.log(j.toString());
 
-let p = new PersistenceManager();
-let filename = "./journal.txt";
+const p = new PersistenceManager();
+const filename = "./journal.txt";
 p.saveToFile(j, filename);
+
+//^ 2. Open-Closed Principle
+console.log("-----------------");
+const Color = Object.freeze({
+  red: "red",
+  green: "green",
+  blue: "blue",
+});
+
+const Size = Object.freeze({
+  small: "small",
+  medium: "medium",
+  large: "large",
+  huge: "huge",
+});
+
+class Product {
+  name: string;
+  color: string;
+  size: string;
+  constructor(name: string, color: string, size: string) {
+    this.name = name;
+    this.color = color;
+    this.size = size;
+  }
+}
+
+// class ProductFilter {
+//   filterByColor(products: Product[], color: any) {
+//     return products.filter((p) => p.color === color);
+//   }
+
+//   filterBySize(products: Product[], size: string) {
+//     return products.filter((p) => p.size === size);
+//   }
+
+//   filterBySizeAndColor(products: Product[], size: string, color: string) {
+//     return products.filter((p) => p.size === size && p.color === color);
+//   }
+
+//   // state space explosion
+//   // 3 criteria (+weight) = 7 methods
+
+//   // OCP = open for extension, closed for modification
+// }
+
+const apple = new Product("Apple", Color.green, Size.small);
+const tree = new Product("Tree", Color.green, Size.large);
+const house = new Product("House", Color.blue, Size.large);
+
+const products = [apple, tree, house];
+
+// const pf = new ProductFilter();
+// console.log(`Green products (old):`);
+// for (let p of pf.filterByColor(products, Color.green))
+//   console.log(` * ${p.name} is green`);
+//* ↑↑↑ BEFORE
+
+//* ↓↓↓ AFTER
+// general interface for a specification
+class ColorSpecification {
+  color: string;
+  constructor(color: string) {
+    this.color = color;
+  }
+
+  isSatisfied(item: Product) {
+    return item.color === this.color;
+  }
+}
+
+class SizeSpecification {
+  size: string;
+  constructor(size: string) {
+    this.size = size;
+  }
+
+  isSatisfied(item: Product) {
+    return item.size === this.size;
+  }
+}
+
+class BetterFilter {
+  filter(items: Product[], spec: ColorSpecification | SizeSpecification | AndSpecification) {
+    return items.filter((x) => spec.isSatisfied(x));
+  }
+}
+
+// specification combinator
+class AndSpecification {
+  specs: (ColorSpecification | SizeSpecification)[];
+  constructor(...specs: (SizeSpecification | ColorSpecification)[]) {
+    this.specs = specs;
+  }
+
+  isSatisfied(item: Product) {
+    return this.specs.every((x) => x.isSatisfied(item));
+  }
+}
+
+const bf = new BetterFilter();
+console.log(`Green products (new):`);
+for (let p of bf.filter(products, new ColorSpecification(Color.green))) {
+  console.log(` * ${p.name} is green`);
+}
+
+console.log(`Large products:`);
+for (let p of bf.filter(products, new SizeSpecification(Size.large))) {
+  console.log(` * ${p.name} is large`);
+}
+
+console.log(`Large and green products:`);
+let spec = new AndSpecification(new ColorSpecification(Color.green), new SizeSpecification(Size.large));
+for (let p of bf.filter(products, spec)) console.log(` * ${p.name} is large and green`);
