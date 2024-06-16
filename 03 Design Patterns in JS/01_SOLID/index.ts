67790291b9bad2bb0f7b7d104d2e0429e60a9daf
Michal Radomski <michalra@op.pl>
2024-06-16 19:23:11 +0200

    Design Patterns in JS - commit 11

--- 03 Design Patterns in JS/01_SOLID/index.ts
+++ 03 Design Patterns in JS/01_SOLID/index.ts
@@ -348,3 +348,86 @@ humanWorker.eat();
 
 const robotWorker = new RobotWorker();
 robotWorker.work();
+
+//^ 4. Dependency Inversion Principle
+console.log("5. -----------------");
+
+const Relationship = Object.freeze({
+  parent: 0,
+  child: 1,
+  sibling: 2,
+});
+
+class Person {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+// LOW-LEVEL (STORAGE)
+class RelationshipBrowser {
+  constructor() {
+    if (this.constructor.name === "RelationshipBrowser") throw new Error("RelationshipBrowser is abstract!");
+  }
+  findAllChildrenOf(_name: string) {}
+}
+
+class Relationships extends RelationshipBrowser {
+  data: { from: Person; type: number; to: Person }[];
+  constructor() {
+    super();
+    this.data = [];
+  }
+
+  addParentAndChild(parent: Person, child: Person) {
+    this.data.push({
+      from: parent,
+      type: Relationship.parent,
+      to: child,
+    });
+    this.data.push({
+      from: child,
+      type: Relationship.child,
+      to: parent,
+    });
+  }
+
+  findAllChildrenOf(name: string) {
+    return this.data.filter((r) => r.from.name === name && r.type === Relationship.parent).map((r) => r.to);
+  }
+}
+
+// HIGH-LEVEL (RESEARCH)
+
+class Research {
+  // constructor(relationships)
+  // {
+  //   // problem: direct dependence ↓↓↓↓ on storage mechanic
+  //   let relations = relationships.data;
+  //   for (let rel of relations.filter(r =>
+  //     r.from.name === 'John' &&
+  //     r.type === Relationship.parent
+  //   ))
+  //   {
+  //     console.log(`John has a child named ${rel.to.name}`);
+  //   }
+  // }
+
+  constructor(browser: Relationships) {
+    for (let p of browser.findAllChildrenOf("John")) {
+      console.log(`John has a child named ${p.name}`);
+    }
+  }
+}
+
+const parent = new Person("John");
+const child1 = new Person("Chris");
+const child2 = new Person("Matt");
+
+// low-level module
+const rels = new Relationships();
+rels.addParentAndChild(parent, child1);
+rels.addParentAndChild(parent, child2);
+
+new Research(rels);
