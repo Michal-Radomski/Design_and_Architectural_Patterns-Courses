b74e26cbe8b5fecb1f8a7b9e3069d0bd3419fec0
Michal Radomski <michalra@op.pl>
2024-06-17 00:36:42 +0200

    Design Patterns in JS - commit 20

--- 03 Design Patterns in JS/02_factory.js
+++ 03 Design Patterns in JS/02_factory.js
@@ -62,3 +62,95 @@ console.log({ p2 });
 // this line will not work if newCartesianPoint is static!
 const p3 = Point.factory.newCartesianPoint(2, 3);
 console.log({ p3 });
+
+//* Example 2
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+// console.log("rl:", rl);
+
+class HotDrink {
+  consume() {}
+}
+
+class Tea extends HotDrink {
+  consume() {
+    console.log("This tea is nice with lemon!");
+  }
+}
+
+class Coffee extends HotDrink {
+  consume() {
+    console.log(`This coffee is delicious!`);
+  }
+}
+
+class HotDrinkFactory {
+  prepare(amount) {
+    /* abstract */
+  }
+}
+
+class TeaFactory extends HotDrinkFactory {
+  prepare(amount) {
+    console.log(`Grind some beans, boil water, pour ${amount}ml`);
+    return new Coffee();
+  }
+}
+
+class CoffeeFactory extends HotDrinkFactory {
+  prepare(amount) {
+    console.log(`Put in tea bag, boil water, pour ${amount}ml`);
+    return new Tea();
+  }
+}
+
+const AvailableDrink = Object.freeze({
+  coffee: CoffeeFactory,
+  tea: TeaFactory,
+});
+
+class HotDrinkMachine {
+  constructor() {
+    this.factories = {};
+    for (let drink in AvailableDrink) {
+      this.factories[drink] = new AvailableDrink[drink]();
+    }
+  }
+
+  makeDrink(type) {
+    switch (type) {
+      case "tea":
+        return new TeaFactory().prepare(200);
+      case "coffee":
+        return new CoffeeFactory().prepare(50);
+      default:
+        throw new Error(`Don't know how to make ${type}`);
+    }
+  }
+
+  interact(consumer) {
+    rl.question("Please specify drink and amount " + "(e.g., tea 50): ", (answer) => {
+      let parts = answer.split(" ");
+      let name = parts[0];
+      let amount = parseInt(parts[1]);
+      let d = this.factories[name].prepare(amount);
+      rl.close();
+      consumer(d);
+    });
+  }
+}
+
+const machine = new HotDrinkMachine();
+// rl.question('which drink? ', function(answer)
+// {
+//   let drink = machine.makeDrink(answer);
+//   drink.consume();
+//
+//   rl.close();
+// });
+machine.interact(function (drink) {
+  drink.consume();
+});
