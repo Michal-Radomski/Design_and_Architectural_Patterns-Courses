b2737d244da06fe9426a159f289e8b320ffad36f
Michal Radomski <michalra@op.pl>
2024-06-22 00:56:05 +0200

    Design Patterns in JS - commit 43

--- 03 Design Patterns in JS/07_composite.js
+++ 03 Design Patterns in JS/07_composite.js
@@ -145,3 +145,28 @@ console.log("neuron1.toString():", neuron1.toString());
 console.log("neuron2.toString():", neuron1.toString());
 console.log("layer1.toString():", neuron1.toString());
 console.log("layer2.toString():", neuron1.toString());
+
+//* Exercise
+class SingleValue {
+  constructor(value) {
+    this.value = value;
+  }
+
+  [Symbol.iterator]() {
+    let returned = false;
+    return {
+      next: () => ({
+        value: this.value,
+        done: returned++,
+      }),
+    };
+  }
+}
+
+class ManyValues extends Array {}
+
+let sum = function (containers) {
+  let result = 0;
+  for (let c of containers) for (let i of c) result += i;
+  return result;
+};
