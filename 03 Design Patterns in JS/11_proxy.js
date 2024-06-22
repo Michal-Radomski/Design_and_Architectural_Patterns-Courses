abda29049bf9962efef1c421e8603ba4c2505d09
Michal Radomski <michalra@op.pl>
2024-06-22 22:18:55 +0200

    Design Patterns in JS - commit 59

--- 03 Design Patterns in JS/11_proxy.js
+++ 03 Design Patterns in JS/11_proxy.js
@@ -109,5 +109,51 @@ function drawImage(img) {
   console.log("Done drawing the image");
 }
 
-let img = new LazyImage("http://pokemon.com/pikachu.png");
+const img = new LazyImage("http://pokemon.com/pikachu.png");
 drawImage(img);
+
+//* Exercise
+class Person {
+  constructor(age = 0) {
+    this.age = age;
+  }
+
+  drink() {
+    return "drinking";
+  }
+  drive() {
+    return "driving";
+  }
+  drinkAndDrive() {
+    return "driving while drunk";
+  }
+}
+
+class ResponsiblePerson {
+  constructor(person) {
+    this.person = person;
+  }
+
+  get age() {
+    return this.person.age;
+  }
+
+  set age(value) {
+    this.person.age = value;
+  }
+
+  drink() {
+    if (this.age >= 18) return this.person.drink();
+    return "too young";
+  }
+
+  drive() {
+    if (this.age >= 16) return this.person.drive();
+    return "too young";
+  }
+
+  // don't do it, folks!
+  drinkAndDrive() {
+    return "dead";
+  }
+}
