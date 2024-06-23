30e78e5a8afadd1f23d91ae400964500c1b19152
Michal Radomski <michalra@op.pl>
2024-06-23 19:42:43 +0200

    Design Patterns in JS - commit 72

---
+++ 03 Design Patterns in JS/16_mediator.js
@@ -0,0 +1,58 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.chatLog = [];
+  }
+
+  receive(sender, message) {
+    let s = `${sender}: '${message}'`;
+    console.log(`[${this.name}'s chat session] ${s}`);
+    this.chatLog.push(s);
+  }
+
+  say(message) {
+    this.room.broadcast(this.name, message);
+  }
+
+  pm(who, message) {
+    this.room.message(this.name, who, message);
+  }
+}
+
+class ChatRoom {
+  constructor() {
+    this.people = [];
+  }
+
+  broadcast(source, message) {
+    for (let p of this.people) if (p.name !== source) p.receive(source, message);
+  }
+
+  join(p) {
+    let joinMsg = `${p.name} joins the chat`;
+    this.broadcast("room", joinMsg);
+    p.room = this;
+    this.people.push(p);
+  }
+
+  message(source, destination, message) {
+    for (let p of this.people) if (p.name === destination) p.receive(source, message);
+  }
+}
+
+const room = new ChatRoom();
+
+const john = new Person("John");
+const jane = new Person("Jane");
+
+room.join(john);
+room.join(jane);
+
+john.say("hi room");
+jane.say("oh, hey john");
+
+const simon = new Person("Simon");
+room.join(simon);
+simon.say("hi everyone!");
+
+jane.pm("Simon", "glad you could join us!");
