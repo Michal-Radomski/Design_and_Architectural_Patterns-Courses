269da0241e34a62db80a1387d0d226bb7d55c7eb
Michal Radomski <michalra@op.pl>
2024-06-30 13:50:47 +0200

    Frontend Architecture and Patterns course - commit 12

---
+++ 04 Architecture/000_React/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.scss";
+
+const App = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <h1>State Management in a React SPA</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
