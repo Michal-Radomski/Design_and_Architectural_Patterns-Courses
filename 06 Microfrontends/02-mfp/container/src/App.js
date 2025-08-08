7ae85111be78385d14b8e7f9f80ba6e32a6aa519
Michal Radomski <michalra@op.pl>
2025-08-08 23:36:17 +0200

    Microfrontends course - commit 15

--- 06 Microfrontends/02-mfp/container/src/App.js
+++ 06 Microfrontends/02-mfp/container/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <React.Fragment>
-      <div>
-        <h1>Hi there!</h1>
-        <hr />
-        <MarketingApp />
-      </div>
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header />
+            {/* <h1>Hi there!</h1>
+            <hr /> */}
+            <MarketingApp />
+          </div>
+        </StylesProvider>
+      </BrowserRouter>
     </React.Fragment>
   );
 };
