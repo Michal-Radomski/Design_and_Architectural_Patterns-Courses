9890aaf6faded261990abd013ef5d6d6b8846422
Michal Radomski <michalra@op.pl>
2025-08-09 21:48:59 +0200

    Microfrontends course - commit 16

--- 06 Microfrontends/02-mfp/marketing/src/bootstrap.js
+++ 06 Microfrontends/02-mfp/marketing/src/bootstrap.js
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
 import App from "./App";
 
 // Mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(
     <React.Fragment>
-      {/* <h1>Hi there!</h1> */}
-      <App />
+      <App history={history} />
     </React.Fragment>,
     el
   );
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation,
@@ -20,7 +36,7 @@ if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
