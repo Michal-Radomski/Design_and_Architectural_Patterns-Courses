c4bdb877167ce8c530aa29cf5e984ab9104c214e
Michal Radomski <michalra@op.pl>
2025-08-09 22:25:28 +0200

    Microfrontends course - commit 18

---
+++ 06 Microfrontends/02-mfp/container/src/components/AuthApp.js
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+import { mount } from "auth/AuthApp";
+
+export default () => {
+  const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      initialPath: history.location.pathname,
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div ref={ref} />
+    </React.Fragment>
+  );
+};
