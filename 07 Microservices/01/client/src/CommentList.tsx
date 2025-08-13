6fe13f96e0c5c329d828688f6e7497cd6a0fd3ac
Michal Radomski <michalra@op.pl>
2025-08-13 23:04:13 +0200

    Microservices with Node JS and React course - commit 10

--- 07 Microservices/01/client/src/CommentList.tsx
+++ 07 Microservices/01/client/src/CommentList.tsx
@@ -3,8 +3,22 @@ import React from "react";
 import type { CommentByPostId } from "@common/CommonInterfaces";
 
 const CommentList = ({ comments }: { comments: CommentByPostId[] }): JSX.Element => {
-  const renderedComments: JSX.Element[] = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+  const renderedComments: JSX.Element[] = comments.map((comment: CommentByPostId) => {
+    let content!: string;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return (
--- 07 Microservices/01/event-bus/index.ts
+++ 07 Microservices/01/event-bus/index.ts
@@ -62,6 +62,9 @@ app.post("/events", (req: Request, res: Response) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log("err.message:", err.message);
   });
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log("err.message:", err.message);
+  });
   res.send({ status: "OK" });
 });
 
