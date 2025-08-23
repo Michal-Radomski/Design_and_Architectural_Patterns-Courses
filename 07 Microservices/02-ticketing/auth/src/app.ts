1e3b42bca0f9e310885ce6d990f6239e8038c422
Michal Radomski <michalra@op.pl>
2025-08-23 20:57:06 +0200

    Microservices with Node JS and React course - commit 37

---
+++ 07 Microservices/02-ticketing/auth/src/app.ts
@@ -0,0 +1,81 @@
+import path from "path";
+import http from "http";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+// import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
+
+//* The server
+const app: Express = express();
+
+const corsOptions = {
+  origin: true,
+  methods: ["POST", "GET", "OPTIONS"],
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization", "Accept"], // Specify
+};
+
+//* Middlewares
+app.set("trust proxy", true);
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false,
+  })
+);
+// Compress all responses
+app.use(compression({ level: 6 }));
+app.use(
+  cookieSession({
+    signed: false,
+    // secure: true,
+  })
+);
+
+//* Favicon
+app.get("/favicon.ico", (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname + "/favicon.svg"));
+});
+//* Test route
+app.get("/test", (req: Request, res: Response) => {
+  console.log("req.ip:", req.ip);
+  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
+});
+
+app.use(errorHandler);
+// app.all("/*splat", async (_req, _res, next) => {
+//   next(new NotFoundError());
+// });
+// app.all("/*splat", async (_req, _res) => {
+//   throw new NotFoundError();
+// });
+
+//^ Routes
+app.use("", currentUserRouter);
+app.use("", signinRouter);
+app.use("", signoutRouter);
+app.use("", signupRouter);
+
+//* HTTP Server
+export const httpServer = http.createServer(app);
--- 07 Microservices/02-ticketing/auth/src/index.ts
+++ 07 Microservices/02-ticketing/auth/src/index.ts
@@ -1,91 +1,13 @@
-import path from "path";
-import http from "http";
-
+import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
-import express, { Express, Request, Response } from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import helmet from "helmet";
-import compression from "compression";
-import mongoose from "mongoose";
-import cookieSession from "cookie-session";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-// import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middlewares/error-handler";
-
-//* The server
-const app: Express = express();
-
-const corsOptions = {
-  origin: true,
-  methods: ["POST", "GET", "OPTIONS"],
-  preflightContinue: false,
-  optionsSuccessStatus: 200,
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "Accept"], // Specify
-};
-
-//* Middlewares
-app.set("trust proxy", true);
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("combined"));
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-    crossOriginResourcePolicy: false,
-    crossOriginEmbedderPolicy: false,
-    crossOriginOpenerPolicy: false,
-  })
-);
-// Compress all responses
-app.use(compression({ level: 6 }));
-app.use(
-  cookieSession({
-    signed: false,
-    // secure: true,
-  })
-);
-
-//* Favicon
-app.get("/favicon.ico", (_req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname + "/favicon.svg"));
-});
-//* Test route
-app.get("/test", (req: Request, res: Response) => {
-  console.log("req.ip:", req.ip);
-  res.send("<h1 style='color:blue;text-align:center'>API is running</h1>");
-});
-
-app.use(errorHandler);
-// app.all("/*splat", async (_req, _res, next) => {
-//   next(new NotFoundError());
-// });
-// app.all("/*splat", async (_req, _res) => {
-//   throw new NotFoundError();
-// });
-
-//^ Routes
-app.use("", currentUserRouter);
-app.use("", signinRouter);
-app.use("", signoutRouter);
-app.use("", signupRouter);
-
-//* Port
-const portHTTP = (process.env.HTTP_PORT || 3000) as number;
-
-//* HTTP Server
-const httpServer = http.createServer(app);
-//* IPv4
+import { httpServer } from "./app";
 
 (async function start(): Promise<void> {
+  //* Port
+  const portHTTP = (process.env.HTTP_PORT || 3000) as number;
+
   const JWT_KEY = process.env.JWT_KEY as string;
 
   if (!JWT_KEY) {
@@ -101,6 +23,7 @@ const httpServer = http.createServer(app);
     })
     .catch((error: string) => console.log("Mongo DB Error => ", error));
 
+  //* IPv4
   httpServer.listen({ port: portHTTP, host: "127.0.0.1" }, () => {
     console.log(`🚀 Server is listening at http://localhost:${portHTTP}`);
     // For testing only
