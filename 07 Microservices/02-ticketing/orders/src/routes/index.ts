1e8a21df958846dde85ce46ebe0b8294a932669d
Michal Radomski <michalra@op.pl>
2025-09-01 22:34:09 +0200

    Microservices with Node JS and React course - commit 67

--- 07 Microservices/02-ticketing/orders/src/routes/index.ts
+++ 07 Microservices/02-ticketing/orders/src/routes/index.ts
@@ -1,9 +1,16 @@
-import express, { Request, Response, Router } from "express";
+import express, { Request, RequestHandler, Response, Router } from "express";
+
+import { requireAuth } from "@rallycoding/common";
+import { Order } from "../models/order";
 
 const router: Router = express.Router();
 
-router.get("/api/orders", async (_req: Request, res: Response) => {
-  res.send({});
+router.get("/api/orders", requireAuth as unknown as RequestHandler, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate("ticket");
+
+  res.send(orders);
 });
 
 export { router as indexOrderRouter };
--- 07 Microservices/02-ticketing/orders/src/routes/show.ts
+++ 07 Microservices/02-ticketing/orders/src/routes/show.ts
@@ -1,9 +1,21 @@
-import express, { Request, Response, Router } from "express";
+import express, { Request, RequestHandler, Response, Router } from "express";
+
+import { NotAuthorizedError, NotFoundError, requireAuth } from "@rallycoding/common";
+import { Order } from "../models/order";
 
 const router: Router = express.Router();
 
-router.get("/api/orders/:orderId", async (_req: Request, res: Response) => {
-  res.send({});
+router.get("/api/orders/:orderId", requireAuth as unknown as RequestHandler, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.orderId).populate("ticket");
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  res.send(order);
 });
 
 export { router as showOrderRouter };
