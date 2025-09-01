66e79b20a7ca5dad3219dd28299cf85ca7359b78
Michal Radomski <michalra@op.pl>
2025-09-01 21:40:17 +0200

    Microservices with Node JS and React course - commit 65

--- 07 Microservices/02-ticketing/orders/src/routes/new.ts
+++ 07 Microservices/02-ticketing/orders/src/routes/new.ts
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 import express, { Request, RequestHandler, Response, Router } from "express";
 import { body } from "express-validator";
 
-import { requireAuth, validateRequest } from "@rallycoding/common";
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@rallycoding/common";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router: Router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS: number = 15 * 60;
 
 router.post(
   "/api/orders",
@@ -17,8 +21,37 @@ router.post(
       .withMessage("TicketId must be provided"),
   ],
   validateRequest as unknown as RequestHandler,
-  async (_req: Request, res: Response) => {
-    res.send({});
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    const isReserved: boolean = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+
+    // Publish an event saying that an order was created
+
+    res.status(201).send(order);
   }
 );
 
